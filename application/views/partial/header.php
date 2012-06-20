557b984202523679b5bde128226373e178c19390
vj <vijay@phppointofsale.com>
2012-06-20 13:36:57 +0530

    quick start
    basic start

---
+++ application/views/partial/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	
+	<title></title>
+	<link rel="icon" href="<?php echo base_url();?>favicon.ico" type="image/x-icon"/>
+<!--css files -->
+
+	<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/general.css" />
+<!--js files -->
+
+
+
+<script src="<?php echo base_url();?>js/jquery-1.3.2.min.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
