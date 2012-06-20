20119e869cb7110bbe373b5a93b3ce7d251c0750
vj <vijay@phppointofsale.com>
2012-06-20 23:59:40 +0530

    validations for add customer

--- application/views/add_customer.php
+++ application/views/add_customer.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Add Customer</title>
-</head>
-
-<body>
-<?php echo form_open('customer/create_customer')?>
-
-<center><h3>Add Customer</h3>
-<table><tr><td> Customer ID: </td>
-<td> <?php echo form_input('pid','')?> </td></tr>
+<?php $this->load->view('partial/header');
+ echo form_open('customer/create_customer',array('id'=>'add_customer'))?>
+<center><ul type='none' id="error_message_box"></ul>
+<h3>Add Customer</h3>
+<ul id="error_message_box"></ul>
+<table> 	
 <tr><td> First Name </td>
 <td> <?php echo form_input('first_name','')?> </td></tr>
 <tr><td> Last Name </td>
@@ -38,4 +31,58 @@
 <tr><td> <?php echo form_submit('submit','Add Customer')?></td>
 <td><?php echo form_reset('reset','Reset')?></td></tr>
 </table></center>
-</html>
+
+<script type='text/javascript'>
+
+//validation and submit handling
+$(document).ready(function()
+{	
+	var submitting = false;
+	
+	$('#add_customer').validate({
+		submitHandler:function(form)
+		{
+			if (submitting) return;
+			submitting = true;
+			$(form).mask("Loading Please wait");
+			$(form).ajaxSubmit({
+			success:function(response)
+			{
+				alert('success');
+				//window.location.href='<?php anchor('login'); ?>';
+			},
+			dataType:'json'
+		});
+
+		},
+		errorLabelContainer: "#error_message_box",
+ 		wrapper: "li",
+		rules: 
+		{
+			first_name:"required",
+			last_name:"required",
+			phone_number:
+			{
+			required:true,
+			minlength:10
+			},
+			installation_date:"required",
+			payment_type:"required",
+			amount:"required"
+   		},
+		messages: 
+		{   
+			first_name:"First Name is required",
+			last_name:"Last Name is required",
+			phone_number:
+			{
+			required:"Phone Number is required",
+			minlength:"Phone number should be minimum 10"
+			},
+			installation_date:"Installation is required",
+			payment_type:"Payment Type is required",
+			amount:"Amount is required"
+		}
+	});
+});
+</script>
--- application/views/add_employee.php
+++ application/views/add_employee.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Add Employee</title>
-</head>
-
-<body>
-<?php echo form_open('employee/create_employee')?>
-
+<?php
+$this->load->view('partail/header');
+ echo form_open('employee/create_employee')?>
 <center><h3>Add Employee</h3>
 <table><tr><td> Employee ID: </td>
 <td> <?php echo form_input('pid','')?> </td></tr>
